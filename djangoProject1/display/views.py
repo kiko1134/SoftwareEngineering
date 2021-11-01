8c4e5eaa73ace2fd253277ae7e0bb3f1f381b1ea
kiko1134 <krisvas8aelsys@gmail.com>
2021-11-01 19:58:59 +0200

    Added minor correction

--- djangoProject1/display/views.py
+++ djangoProject1/display/views.py
@@ -6,7 +6,7 @@ import requests
 
 def index(request):
     response = requests.get("https://api-v3.mbta.com/predictions?page%5Boffset%5D=0&page%5Blimit%5D=10&sort=departure_time&include=schedule%2Ctrip&filter%5Bdirection_id%5D=0&filter%5Bstop%5D=place-north")
-
+    # r = response.json()
     r = response.json()
     rr = r['data']
     rr_included = r['included']
